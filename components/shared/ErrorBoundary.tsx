9e5bac3f4b743e2708ee8ac335b229bfdd313a59
Ryan Vandehey <vandehey12@gmail.com>
2024-10-22 10:34:49 -0500

    error boundary on contact

--- components/shared/ErrorBoundary.tsx
+++ components/shared/ErrorBoundary.tsx
@@ -1,30 +1,30 @@
-import React, { ErrorInfo, ReactNode } from 'react'
+'use client'
+
+import React, { Component, ErrorInfo, ReactNode } from 'react'
 
 interface ErrorBoundaryProps {
   children: ReactNode
+  fallback?: ReactNode
 }
 
 interface ErrorBoundaryState {
   hasError: boolean
 }
 
-class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props)
-    this.state = { hasError: false }
-  }
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  state = { hasError: false }
 
   static getDerivedStateFromError(_: Error): ErrorBoundaryState {
     return { hasError: true }
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error("Uncaught error:", error, errorInfo)
+    console.error('Uncaught error:', error, errorInfo)
   }
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>
+      return this.props.fallback || <h1>Something went wrong.</h1>
     }
 
     return this.props.children
